c85cbb9669a1b26693f408c16b593925e3268ac0
daniel.gladstone <gladstone.d@gmail.com>
2020-12-04 14:32:07 -0500

    navbar

---
+++ frontend/app/src/common/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="navbar border-bottom">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <span className="navbar-brand"><h1>Vendee Tracker</h1></span>
+            </div>
+        </div>
+        </nav>
+    )
+}
+
+export default Header
