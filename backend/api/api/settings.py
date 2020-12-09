27c11fd3a138b2f05d0174e36db772e00392e4bd
daniel.gladstone <gladstone.d@gmail.com>
2020-12-09 18:00:19 -0500

    update db settings

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dc1h44eciqsfme',
+        'PASSWORD': os.getenv('SOCIAL_DB_PASS'),
+        'USER': 'qkokzxvezoboeo',
+        'HOST': 'ec2-18-206-103-49.compute-1.amazonaws.com'
     }
 }
 
