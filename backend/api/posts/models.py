81fcb28b2da137fcd968e4d541f44a5c7de8da1d
daniel.gladstone <gladstone.d@gmail.com>
2020-12-09 16:50:05 -0500

    heroku and models setup

--- backend/api/posts/models.py
+++ backend/api/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    PLATFORM_CHOICES = [
+        ('youtube', 'youtube')
+    ]
+    platform = models.CharField(
+        max_length=50,
+        choices=PLATFORM_CHOICES,
+    )
+    created = models.DateTimeField()
+    collected = models.DateTimeField(auto_now=True)
+    title = models.CharField(null=True, blank=True, max_length=500)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    soruce_id = models.CharField(max_length=500, null=True, blank=True)
