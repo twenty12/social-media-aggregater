39c1e11e1066d247d14cdc70af1cb49d8634a5e4
daniel.gladstone <gladstone.d@gmail.com>
2020-12-20 19:11:28 -0500

    update scraper

--- backend/api/posts/management/scrapers/youtube.py
+++ backend/api/posts/management/scrapers/youtube.py
@@ -136,7 +136,7 @@ def _video_item_dict_to_post(video, account):
         created=video['snippet']['publishTime'],
         source_id=video['id']['videoId'],
         url='https://www.youtube.com/watch?v=' + video['id']['videoId'],
-        thumbnail=video['thumbnails']['default']['url']
+        thumbnail=video['snippet']['thumbnails']['default']['url']
     )
     print('Adding {}\'s  -  {}'.format(
         account.name,
