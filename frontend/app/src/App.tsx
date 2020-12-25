0277dfe3357521a4d5e8e232919de81e394edd3b
daniel.gladstone <gladstone.d@gmail.com>
2020-12-25 15:35:56 -0500

    rout basics

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -6,17 +6,34 @@ import { loadPosts } from "./redux/modules/posts"
 import {useDispatch, useSelector} from "react-redux"
 import './index.css';
 import { RootState } from "./redux/reducer"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
+  
 function App() {
     const dispatch = useDispatch()
     const pageNumber = useSelector((state: RootState) => state.post.pageNumber)
 
     return (
-        <div>
-            <NavBar />
+        <Router>
+
             {/* <button onClick={() => dispatch(loadPosts(pageNumber) as any)}>Load em</button> */}
-            <Feed />
-        </div>
+            <Switch>
+
+                <Route path="/race/:raceSlug">
+                    <NavBar />
+                    <Feed />
+                </Route>
+                <Route path="/">
+                    <Link to="race/vendee">vendee</Link>
+                    <h1>home</h1>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
--- frontend/app/src/features/Header.tsx
+++ frontend/app/src/features/Header.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import {useParams} from "react-router-dom"
+interface ParamTypes {
+    raceSlug: string
+  }
 
+const raceSlugToName: { [key: string]: string; } = {
+    'vendee-globe': 'Vendee Globe'
+}
 export default function NavBar () {
+    const { raceSlug } = useParams<ParamTypes>()
     const createdTag = <small>By <a className="text-black" href="http://danielgladstone.com">Daniel Gladstone</a></small>
     return (
         <nav className="navbar navbar-default border-bottom">
                 <div className="w-100">
                     <div className="d-flex">
                         <span className="navbar-brand text-wrap">
-                            <h1>Vendée Globe Tracker</h1>
+                            <h1>{raceSlugToName[raceSlug]}</h1>
                         </span>
                     </div>
                     <div className="d-flex w-100 align-items-end">
